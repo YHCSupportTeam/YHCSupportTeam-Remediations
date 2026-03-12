ea9a8f9d4d450b8d8f69885c14aa5e2379ad2fb0
YHCSupportTeam <125400820+YHCSupportTeam@users.noreply.github.com>
2026-03-12 07:54:11 -0400

    Delete Remediate-WinGetUpgrade.ps1

--- Remediate-WinGetUpgrade.ps1
+++
@@ -1,7 +0,0 @@
-# Remediation script to silently upgrade all winget packages
-try {
-    Start-Process "winget" -ArgumentList "upgrade --all --silent" -NoNewWindow -Wait
-    exit 0
-} catch {
-    exit 1
-}
